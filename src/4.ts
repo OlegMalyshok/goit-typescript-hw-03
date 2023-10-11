8158c52864d3095e1ad44deb83ebed0be1f8728d
OlegMalyshok <olegmalyshok@gmail.com>
2023-10-11 20:16:29 +0300

    upd

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,61 @@
+class Key {
+  private password: number;
+
+  constructor() {
+    this.password = Math.random();
+  }
+
+  getPassword(): number {
+    return this.password;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Ви зайшли в будинок.");
+      return true;
+    } else {
+      console.log("Двері зачинені");
+      return false;
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key) {
+    if (key.getPassword() === this.key.getPassword()) {
+      this.door = true;
+      console.log("Двері відчинені.");
+      return true;
+    } else {
+      console.log("Неправильний ключ");
+      return false;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +64,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
